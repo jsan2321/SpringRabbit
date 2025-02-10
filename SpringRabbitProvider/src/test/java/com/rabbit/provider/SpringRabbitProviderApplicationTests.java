4fdcdb5e732e7e7aa090e19d19585439a8b3c562
jsan2321 <jsantiago.c@hotmail.com>
2025-02-10 13:29:54 -0500

    initial commit

---
+++ SpringRabbitProvider/src/test/java/com/rabbit/provider/SpringRabbitProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbit.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
